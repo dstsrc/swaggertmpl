b96540e3120c6f41a83985c9443e06a13ffdb985
Alexey Galyshev <aleksey.galyshev@myoffice.team>
2020-05-24 14:15:18 +0300

    swagger

--- Makefile
+++ Makefile
@@ -14,3 +14,9 @@ generate-swagger:
 	protoc -I/usr/local/include -I. -I${GOPATH}/src \
 	-I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
 	--swagger_out=logtostderr=true:. pkg/pb/service.proto
+
+gen-statik:
+	rm -rf statik/
+	rm -f swaggerui/service.swagger.json
+	cp pkg/pb/service.swagger.json swaggerui/
+	statik -src swaggerui
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,12 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strings"
+
+	"github.com/golang/glog"
+
+	_ "github.com/dstsrc/swaggertmpl/statik"
+	"github.com/rakyll/statik/fs"
 
 	"github.com/dstsrc/swaggertmpl/internal/app"
 
@@ -26,18 +32,50 @@ func run() error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	httpmux := http.NewServeMux()
+	statikFS, err := fs.New()
+	if err != nil {
+		panic(err)
+	}
+	staticServer := http.FileServer(statikFS)
+	sh := http.StripPrefix("/swaggerui/", staticServer)
+	httpmux.Handle("/swaggerui/", sh)
+
 	// Register gRPC server endpoint
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
-	err := example.RegisterYourServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	err = example.RegisterYourServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
 	if err != nil {
 		return err
 	}
 
+	httpmux.Handle("/", mux)
+
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
 	log.Println("http start")
-	return http.ListenAndServe(":8081", mux)
+	return http.ListenAndServe(":8081", (httpmux))
+}
+
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	glog.Infof("preflight request for %s", r.URL.Path)
+	return
 }
 
 func main() {
